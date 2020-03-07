df91a3d89e7790db909cb9bead65d783c66d5315
liyonghui <liyonghuiwangyi@163.com>
2020-03-07 20:43:21 +0800

    add check cookie

--- src/main/java/com/bilibili/forum/mapper/UserMapper.java
+++ src/main/java/com/bilibili/forum/mapper/UserMapper.java
@@ -3,10 +3,15 @@ package com.bilibili.forum.mapper;
 import com.bilibili.forum.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("INSERT INTO user (name,account_id,token,gmt_create,gmt_modified) value (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
     public void insert(User user);
+
+    @Select("SELECT * FROM user WHERE token = #{token}")
+    User findByToken(@Param("token")String token);
 }
